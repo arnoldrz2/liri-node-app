fc89920ee6c6687c06e5095707ceade7a0e83ea0
arnold <arnoldrz2@gmail.com>
2018-01-18 17:05:54 -0600

    added twitter function, removed info txt files, and added txt files to gitignore list

--- liri.js
+++ liri.js
@@ -2,16 +2,32 @@
 require("dotenv").config();
 // Add code the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+
 // You should then be able to access your keys information like so
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var spotifyKey = new Spotify(keys.spotify);
 
 // Make it so liri.js can take in one of the following commands:
+console.log("To get started, please enter one of the following commands:
+            'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'");
 
-// my-tweets
+var command = process.argv[2];
+var input = process.argv[3];
 
-// spotify-this-song
+function myTweets(){
+    console.log("Social Media Stuff Incoming!");
+    //Access keys
+    var client = new Twitter(keys.twitter);
 
-// movie-this
+    //Parameters for Twitter Function
+    var params = {screen_name: 'roland_arno',
+                  count: 20};
 
-// do-what-it-says
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets);
+      }
+    });
+};
