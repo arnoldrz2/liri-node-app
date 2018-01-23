237b48b49a877e920db59e623a2e3d99b8379176
arnold <arnoldrz2@gmail.com>
2018-01-23 17:54:08 -0600

    added movie, spotify, and dowhatisay functions

--- liri.js
+++ liri.js
@@ -1,52 +1,143 @@
-// At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
-require("dotenv").config();
 // Add code the code required to import the keys.js file and store it in a variable.
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var fs = require('fs');
 
 // You should then be able to access your keys information like so
-// var spotifyKey = new Spotify(keys.spotify);
+var spotifyKey = new Spotify(keys);
 
-// Make it so liri.js can take in one of the following commands:
-console.log("To get started, please enter one of the following commands: 'my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'");
-
-var command = process.argv[2];
-var input = process.argv[3];
-
-//Switch cases for commands
-switch(command) {
-    case "my-tweets":
-      myTweets();
-    break;
-
-    case "spotify-this-song":
-      spotifyThisSong();
-    break;
-
-    case "movie-this":
-      movieThis();
-    break;
-
-    case "do-what-it-says":
-      doWhatItSays();
-    break;
-}
+//==============================================================================
 
 //Get Tweet Function
-function myTweets(){
+var getMyTweets = function(){
     console.log("Social Media Stuff Incoming!");
     //Access keys
-    var client = new Twitter(keys.twitter);
+    var client = new Twitter(keys);
 
     //Parameters for Twitter Function
-    var params = {screen_name: 'roland_arno',
-                  count: 20};
+    var params = {screen_name: 'roland_arno'
+    };
 
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
       if (!error) {
-        console.log(tweets);
+        for (var i = 0; i < tweets.length; i++) {
+          console.log(tweets[i].created_at);
+          console.log("");
+          console.log(tweets[i].text);
+        }
       }
     });
 };
+
+
+// Writes to the log.txt file
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
+
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+
+      var songs = data.tracks.items;
+
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+      }
+    }
+  );
+};
+
+
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
+
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotton Tomatoes URL: " + jsonData.tomatoURL);
+    }
+  });
+};
+
+
+// Function for running a command based on text file
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    }
+    else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+
+
+//Function Switch
+var pick = function(caseData, functionData) {
+  //Switch cases for commands
+  switch(caseData) {
+      case "my-tweets":
+        getMyTweets();
+        break;
+
+      case "spotify-this-song":
+        getMeSpotify(functionData);
+        break;
+
+      case "movie-this":
+        getMeMovie(functionData);
+        break;
+
+      case "do-what-it-says":
+        doWhatItSays();
+        break;
+      default:
+        console.log("LIRI doesn't know that");
+  }
+};
+
+// Function which takes in command line arguments
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
